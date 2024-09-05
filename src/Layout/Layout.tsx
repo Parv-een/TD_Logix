c72c3cc98d49383aa7bd8a635a026a871b2635ed
Parveen Kaur <105818061+Parv-een@users.noreply.github.com>
2024-09-05 16:08:16 -0600

    changed navlink

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand to="#home">
           <img
             src="./src/assets/road-truckicon.jpg"
             width="50"
@@ -18,16 +19,16 @@ export default function Layout() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Trucks Inventory</Nav.Link>
-            <Nav.Link href="#driver">Crew Members</Nav.Link>
-            <Nav.Link href="#shipment">Shipments</Nav.Link>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/trucks">Trucks Inventory</NavLink>
+            <NavLink to="/driver">Crew Members</NavLink>
+            <NavLink to="/shipment">Shipments</NavLink>
           </Nav>
           <Nav>
-            <Nav.Link href="#logout">Logout</Nav.Link>
-            <Nav.Link eventKey={2} href="#profile">
+            <NavLink to="/login">Logout</NavLink>
+            <NavLink eventKey={2} to="#profile">
               Profile
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/Navigation/Routes.tsx
+++ src/Navigation/Routes.tsx
@@ -3,18 +3,19 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import HomePage from "../Pages/HomePage";
-import LoginPage from "../Pages/LoginPage";
+import HomePage from "../Pages/HomePage/HomePage";
+import LoginPage from "../Pages/LoginPage/LoginPage";
 
 import Layout from "../Layout/Layout";
 
 const routes = createRoutesFromElements(
   <>
+  <Route path= " /" element={<Layout />}></Route>
     <Route path="home" element={<HomePage />}></Route>
 
     <Route path="login" element={<LoginPage />}></Route>
 
-    <Route path="layout" element={<Layout />}></Route>
+ 
 
     <Route path="*" element={<div>Not Found Page</div>}></Route>
   </>
